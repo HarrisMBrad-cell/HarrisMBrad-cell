0ae70e157b83946ed8389fcbabc0124c0e6c2670
HarrisMBrad <bmichaelh13@gmail.com>
2020-12-01 08:51:18 -0500

    ... adding comments

--- src/index.js
+++ src/index.js
@@ -61,47 +61,47 @@ class Game5 extends React.Component { // this class extends thus ES6, parent/chi
   } // closing the class HOC...
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1); // returning selected elements in the array...according to the state history...
+    const current = history[history.length - 1]; // subtracting from the lenghth of the array...
+    const squares = current.squares.slice(); // returning the sq elements to check...
+    if (calculateWinner(squares) || squares[i]) { // evealuation of the squares' state comparing squares value in the array...
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([
+    squares[i] = this.state.xIsNext ? "X" : "O";// xIsnext eqality equilvilant query to sq value...
+    this.setState({ // setting state to this...
+      history: history.concat([ // history being concatinated for sq 
         {
-          squares: squares
+          squares: squares // equivilancy declaration...
         }
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
-    });
-  }
+      ]), // closing array to be cocatinated over...
+      stepNumber: history.length, // stepping threough the length of the array's history...
+      xIsNext: !this.state.xIsNext // statement saying what's not next...
+    }); // closing the handelclick method. 
+  } // closing that function.
 
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0
-    });
-  }
+  jumpTo(step) { // from onClick handler the jumpTo method recalls the steps from xIsnext concatenation to new state of steps. 
+    this.setState({ // step object evaluation checking for state
+      stepNumber: step, // steps being updated...
+      xIsNext: (step % 2) === 0 // set even as there are only two player options. 
+    }); // cloing the step evaluation
+  } // closing the function method. 
 
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const history = this.state.history; // setting state to recall. 
+    const current = history[this.state.stepNumber]; // updating array to be recalled. 
+    const winner = calculateWinner(current.squares); // evaluation for winner function passing current position from map history. 
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
+    const moves = history.map((step, move) => { // moves being checked each step and move...
+      const desc = move ? // looking for he move...
+        'Go to move #' + move : // string for the move
+        'Go to game start'; // string for the start
+      return ( // method to list the moves to key position
+        <li key={move}> 
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
     });
-
+// try catch to check for winner of the game. 
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -109,22 +109,22 @@ class Game5 extends React.Component { // this class extends thus ES6, parent/chi
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
-    return (
+    return ( // starting the return method
       <div className="game">
         <div className="game-board">
-          <Board
+          <Board // passing props to squares to handle the value of that event listener
             squares={current.squares}
             onClick={i => this.handleClick(i)}
-          />
+          /> 
         </div>
-        <div className="game-info">
+        <div className="game-info"> 
           <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
+          <ol>{moves}</ol> 
+        </div> 
       </div>
-    );
+    ); // ending of the built-in return method.
   }
-}
+} // ending of the class function.
 
 ReactDOM.render(<Game5 />, document.getElementById("root"));
 
